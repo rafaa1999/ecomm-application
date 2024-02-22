7842828c078d7378af07c3216fdf42bb237f2e45
rafaa1999 <raferhouma10@gmail.com>
2024-02-22 15:34:13 +0100

    Added security to inventory-service

---
+++ inventory-service/src/main/java/com/rafaa/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.rafaa.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+   @Bean
+   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .headers(h -> h.frameOptions(f -> f.disable()))
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/api/**","/swagger-ui.html","/swagger-ui/**","/v3/**","/h2-console/**").permitAll())
+                .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
+                .build();
+   }
+
+}
