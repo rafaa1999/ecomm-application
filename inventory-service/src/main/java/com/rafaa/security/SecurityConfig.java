fc34dc57cc2e1e7bace98d9193c4ffc5d3519fdc
rafaa1999 <raferhouma10@gmail.com>
2024-02-22 22:22:26 +0100

    Managed frond-end-angular

--- inventory-service/src/main/java/com/rafaa/security/SecurityConfig.java
+++ inventory-service/src/main/java/com/rafaa/security/SecurityConfig.java
@@ -2,9 +2,15 @@ package com.rafaa.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -13,11 +19,24 @@ public class SecurityConfig {
    @Bean
    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .headers(h -> h.frameOptions(f -> f.disable()))
                 .authorizeHttpRequests(ar -> ar.requestMatchers("/api/**","/swagger-ui.html","/swagger-ui/**","/v3/**","/h2-console/**").permitAll())
                 .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
                 .build();
+   }
+
+   @Bean
+   CorsConfigurationSource corsConfigurationSource() {
+       CorsConfiguration configuration = new CorsConfiguration();
+       configuration.setAllowedOrigins(Arrays.asList("*"));
+       configuration.setAllowedMethods(Arrays.asList("*"));
+       configuration.setAllowedHeaders(Arrays.asList("*"));
+       configuration.setExposedHeaders(Arrays.asList("*"));
+       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+       source.registerCorsConfiguration("/**",configuration);
+       return source;
    }
 
 }
